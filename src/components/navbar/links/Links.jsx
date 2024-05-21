b56dea16763b795f26110dcd61aaeb9083cf1326
sandeeppatil1998 <sandeeppatil2317@gmail.com>
2024-05-21 19:42:22 +0530

    add style for navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import styles from './links.module.css'
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
   const links = [
@@ -20,13 +22,23 @@ const Links = () => {
     },
   ];
 
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      )))}
+        <NavLink item={link} key={link.title}/>
+      )))}{
+        session ? (
+          <>
+          {isAdmin && <NavLink item={{title: 'Admin', path:'/admin'}}/>}
+          <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{title: "Login", path: '/login'}}/>
+        )
+      }
     </div>
   );
 };
