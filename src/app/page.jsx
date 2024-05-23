b0a98be3d800410271180f9940eccf3ca72ad92b
sandeeppatil1998 <sandeeppatil2317@gmail.com>
2024-05-23 17:56:57 +0530

    add home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+import styles from './home.module.css'
+
 const Home =() =>{
-  return <div>HOme Page</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts. </h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ducimus amet ea, sequi quod fugiat labore a quia nesciunt harum?</p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src='/brands.png' fill alt='' className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src='/hero.gif' fill alt='' className={styles.heroImg}/>
+      </div>
+    </div>  
+  )
 }
 
 export default Home;
