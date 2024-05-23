a0cc782736f3eefba2ac3c14b6bc17498cef1592
sandeeppatil1998 <sandeeppatil2317@gmail.com>
2024-05-23 19:15:30 +0530

    add about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,39 @@
 import Image from "next/image";
-import styles from './about.module.css'
-const AboutPage = () =>{
-    return(
-        <div>
-            <div className={styles.container}>
-                <Image src='/about.png' alt='' fill/>   
+import styles from "./about.module.css";
+const AboutPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Next</h2>
+        <h1 className={styles.title}>
+          We create digital idear that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+            <div className={styles.box}>
+                <h1>10 k+</h1>
+                <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+                <h1>10 k+</h1>
+                <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+                <h1>10 k+</h1>
+                <p>Year of experience</p>
             </div>
         </div>
-    )
-}
+      </div>
+      <div className={styles.imageContainer}>
+        <Image className={styles.img} src="/about.png" alt="" fill />
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
