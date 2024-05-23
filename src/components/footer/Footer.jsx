15339a51fe1804ee05c98c90a58c446b22a1e802
sandeeppatil1998 <sandeeppatil2317@gmail.com>
2024-05-23 18:44:37 +0530

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
+import styles from './footer.module.css'
+
 const Footer = () =>{
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>NextDev</div>
+            <div className={styles.text}>NextDev Creative Thoughts © All rights reserved.</div>
+        </div>
         );
 }
 
