019c98a0c78260fe35ebd583b5bf8dcf91d4cfee
sandeeppatil1998 <sandeeppatil2317@gmail.com>
2024-05-23 19:31:18 +0530

    add contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+import styles from './contact.module.css'
+
 const ContactPage = () =>{
     return(
-        <div>
-            <h1>Contact Us</h1>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} src='/contact.png' alt='' fill/>
+            </div>
+            <div className={styles.formContainer}>
+                <form action='' className={styles.form}>
+                    <input type="text" placeholder='Full Name' />
+                    <input type="text" placeholder='Email Address' />
+                    <input type="text" placeholder='Phone Number' />
+                    <textarea name="" id="" cols='30' rows='10' placeholder='Message'></textarea>
+                    <button className={styles.button}>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
